197419983bbb01c836ca339905a985e8fcd255c0
K Asante <73370911+kasante1@users.noreply.github.com>
2024-11-27 18:43:11 +0000

    chore: project structure

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, web, App, HttpServer, Responder, HttpResponse};
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -10,9 +10,21 @@ async fn hello(name: web::Path<String>) -> impl Responder {
     format!("Hello {}!", &name)
 }
 
+
+#[get("/health-check")]
+async fn health_check()-> impl Responder {
+    HttpResponse::Ok()
+}
+
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index).service(hello))
+    HttpServer::new(|| 
+        App::new()
+        .service(index)
+        .service(hello)
+        .service(health_check)
+        )
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
