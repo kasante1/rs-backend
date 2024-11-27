d5c0669c428e392fe40f968d5491f651ca0bf083
K Asante <73370911+kasante1@users.noreply.github.com>
2024-11-27 19:04:13 +0000

    chore: structure project

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,6 @@
-use actix_web::{get, web, App, HttpServer, Responder, HttpResponse};
+use rs_backend::run;
 
-#[get("/")]
-async fn index() -> impl Responder {
-    "Hello, World!"
-}
-
-#[get("/{name}")]
-async fn hello(name: web::Path<String>) -> impl Responder {
-    format!("Hello {}!", &name)
-}
-
-
-#[get("/health-check")]
-async fn health_check()-> impl Responder {
-    HttpResponse::Ok()
-}
-
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| 
-        App::new()
-        .service(index)
-        .service(hello)
-        .service(health_check)
-        )
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+#[tokio::main]
+async fn main() -> std::io::Result<()>{
+    run().await
 }
